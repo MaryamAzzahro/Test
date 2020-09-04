924f0561bc2481cb19af49ebcf2db75d16757f48
MaryamAzzahro <maryamazzahro354@gmail.com>
2020-09-04 17:52:22 +0700

    firsCommit

---
+++ android/app/src/main/kotlin/com/satu/satu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.satu.satu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Demo 1',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("profile"),
+      ),
+    );
+  }
+}
